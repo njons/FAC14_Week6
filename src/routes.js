8c0bb1063276be9c2a433d13f6e58bbe3c4a0eff
Nathalie Jonsson <jonsson.nathalie@gmail.com>
2018-08-09 21:23:49 +0100

    server.js and router'js

--- src/routes.js
+++ src/routes.js
@@ -1,1 +1,18 @@
+// what do we need to create the routes
+const path = require('path');
+// what code do we need to access?
+const { publicFilesRoute, getDataRoute, postDataRoute } = require('./handler');
 
+const routes = (request, response) => {
+  const url = request.url;
+
+  if (url === '/') {
+    publicFilesRoute(request, 'index.html');
+  } else if (url === '/about') {
+    publicFilesRoute(request, 'about.html');
+  } else if (url === '/getData') {
+    getDataRoute(request, response);
+  } else if (url === '/postData') {
+    postDataRoute(request, response);
+  }
+}
--- src/server.js
+++ src/server.js
@@ -1,1 +1,12 @@
+// what do we need to create a server
+const http = require('http');
+const port = process.env.PORT || 3000;
+const hostname = process.env.HOSTNAME || 'localhost';
+// what code do we need to access?
+const routes = require('./routes');
 
+
+const server = http.createServer(routes);
+server.listen(port, () => {
+  console.log(`sever is sunning at: http://${hostname}:${port}`);
+})
