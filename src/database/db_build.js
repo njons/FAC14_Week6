468972a266bcf21734d553fb7f7e9f21c35de29f
Nathalie Jonsson <jonsson.nathalie@gmail.com>
2018-08-11 14:32:28 +0100

    adds local database

--- src/database/db_build.js
+++ src/database/db_build.js
@@ -1,1 +1,19 @@
+// BUILD THE DATABASE: to build the database we will call on the connecton details in db_connection and read the SQL file, running it as a string in a .query() method that is able to execute SQL
 
+// what do we need to be able to build the database?
+const fs = require('fs');
+// what code do we need to be able to access?
+const dbConnection = require('./db_connection');
+
+// start by reading the SQL file and converting it into a string
+const sql = fs.readFileSync(`${__dirname}/db_build.sql`).toString();
+console.log(sql)
+
+// write the function to build the database
+dbConnection.query(sql, (err, result) => {
+  if (err) cb(err);
+  console.log(`table was created`, result);
+});
+
+
+// module.exports = runDbBuild;
--- src/database/db_build.sql
+++ src/database/db_build.sql
@@ -13,12 +13,12 @@ CREATE TABLE users (
   -- set data type for text, cannot be empty
   name VARCHAR(50) NOT NULL,
   -- set data type for the birthdate
-  birthdate DATE NOT NULL
+  birthdate DATE NOT NULL,
   -- set data type for the deathdate
-  deathdate DATE 
+  deathdate DATE
 );
 
-INSERT INTO users (name, birthdate) VALUES
+INSERT INTO users (name, birthdate, deathdate) VALUES
 ('Nathalie', '1984-11-13', '2033-06-03'),
 ('Ingmar', '1918-07-14', '2007-07-30'),
 ('Ronja', '1994-07-02', '2011-11-22');
