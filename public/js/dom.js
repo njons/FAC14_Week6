0a2f03dffd22353822cf58c21e8a97127e0b329c
Nathalie Jonsson <jonsson.nathalie@gmail.com>
2018-08-12 12:17:54 +0100

    added random prediction

--- public/js/dom.js
+++ public/js/dom.js
@@ -1,59 +1,91 @@
 //console.log('this is dom.js');
-const months = {
-  '1': 'January',
-  '2': 'February',
-  '3': 'March',
-  '4': 'April',
-  '5': 'May',
-  '6': 'June',
-  '7': 'July',
-  '8': 'August',
-  '9': 'September',
-  '10': 'October',
-  '11': 'November',
-  '12': 'December'
-}
 
+// random number generator
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function getRandomDate(){
+// get the random number generator to calculate numbers for year, month and date, return a full string
+function getRandomYear(){
   var year = getRandomInt(2033, 2084);
-  var monthNum = getRandomInt(1, 12);
-  var monthWord = months[monthNum];
-
-  if (monthNum === 2) {
-    var day = getRandomInt(1, 28);
-  } else if (monthNum === 4 || monthNum === 6 ||  monthNum === 9 || monthNum === 11) {
-    var day = getRandomInt(1, 30);
-  } else {
-    var day = getRandomInt(1, 31);
-  }
-
-  var date = 'Your date is ' + day + ' ' + monthWord + ' ' + year;
-  return date;
+  return year;
 }
 
+function getRandomMonthNum() {
+  var monthNum = getRandomInt(1, 12);
+  return monthNum;
+}
 
-// what code do we need to accsess?
+function getRandomMonthWord() {
+  const months = {
+    '1': 'January',
+    '2': 'February',
+    '3': 'March',
+    '4': 'April',
+    '5': 'May',
+    '6': 'June',
+    '7': 'July',
+    '8': 'August',
+    '9': 'September',
+    '10': 'October',
+    '11': 'November',
+    '12': 'December'
+  }
+  var monthNum = getRandomMonthNum();
+  var monthWord = months[monthNum];
+  return monthWord;
+}
+
+function getRandomDay() {
+  var monthNum = getRandomMonthNum();
+  if (monthNum === 2) {
+    return getRandomInt(1, 28);
+  } else if (monthNum === 4 || monthNum === 6 ||  monthNum === 9 || monthNum === 11) {
+    return  getRandomInt(1, 30);
+  } else {
+    return getRandomInt(1, 31);
+  }
+}
+
+function getRandomDateNum() {
+  var year = getRandomYear();
+  var month = getRandomMonthNum();
+  var day = getRandomDay();
+  var dateNum = year + '-' + month + '-' + day;
+  return dateNum;
+}
+
+function getRandomDateString() {
+  var year = getRandomYear();
+  var month = getRandomMonthWord();
+  var day = getRandomDay();
+  var dateString = 'Your date is ' + day + ' ' + month + ' ' + year;
+  return dateString;
+}
+// call the function that return the full string
+var dateString = getRandomDateString();
+var dateNum = getRandomDateNum();
+
+// target the button
 const submit = document.getElementsByTagName("button");
-
-
+// on click
 submit[0].addEventListener('click', function(e){
   console.log('you clicked the button')
-  e.preventDefault();
-  var finalDate = getRandomDate()
+  // stop page from reloading
+  // e.preventDefault();
+
+  // create div displaying result
+  const div = document.getElementsByTagName('div');
+  const results = document.createElement('p');
+  results.innerText = dateString;
+  div[0].appendChild(results);
 
 
-  const ul = document.getElementsByTagName('ul');
-  const results = document.createElement('li')
-  results.innerText = finalDate;
-  ul[0].appendChild(results);
-
-  console.log('this is final date:', finalDate)
+  const death = document.getElementById('death');
+  death.setAttribute('value', dateNum);
 
 
 
+  console.log('this is dateNum:', dateNum);
 
 })
